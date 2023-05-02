bba73b3b7f6dd62d473f0593ecba5f8ed168a1ae
YuriiSynyavskiy <53538660+YuriiSynyavskiy@users.noreply.github.com>
2023-05-02 14:30:06 +0300

    Modal window, preform submission

--- src/components/AddMarkerModal/index.jsx
+++ src/components/AddMarkerModal/index.jsx
@@ -1,15 +1,10 @@
-import {
-  Button,
-  Form,
-  FormGroup,
-  Input,
-  Label,
-  Modal,
-  ModalBody,
-  ModalFooter,
-  ModalHeader,
-} from 'reactstrap'
+import './index.css'
 import React from 'react'
+import Images from '../../images/'
+import { Button, Form, Input, Select, Modal, Switch, InputNumber } from 'antd'
+
+
+const { TextArea } = Input
 
 const MapModal = ({ isOpen, setClose, markerPosition, createMarker }) => {
   const handleSubmitForm = (e) => {
@@ -26,49 +21,59 @@ const MapModal = ({ isOpen, setClose, markerPosition, createMarker }) => {
 
   return (
     <div>
-      <Modal isOpen={isOpen}>
-        <Form onSubmit={handleSubmitForm}>
-          <ModalHeader>New marker</ModalHeader>
-          <ModalBody>
-            <FormGroup>
-              <Label for='name'>Name</Label>
-              <Input type='text' name='name' id='name' placeholder='Enter marker name' />
-            </FormGroup>
-            <FormGroup>
-              <Label for='description'>Description</Label>
-              <Input type='textarea' name='description' id='description' placeholder='Enter description here' />
-            </FormGroup>
-            <FormGroup>
-              <Label for='positionCount'>Position Count</Label>
-              <Input type='number' name='positionCount' id='positionCount' placeholder='Enter position count'/>
-            </FormGroup>
-            <FormGroup>
-              <Label for='positionType'>Position Type</Label>
-              <Input type='select' name='positionType' style={{display: 'block', borderRadius: '6px', borderColor: 'lightgrey', width: '100%', padding: '8px 0'}}>
-                <option value='1'>
-                  Zhoska position
-                </option>
-                <option value='2'>
-                  Slaba position
-                </option>
-                <option value='3'>
-                  Huyova position
-                </option>
-              </Input>
-            </FormGroup>
-          </ModalBody>
-          <ModalFooter>
-            <Button type='submit' color='primary'>
-              Save
-            </Button>
-            <Button type='reset' color='secondary'>
-              Discard
-            </Button>
-            <Button color='danger' onClick={setClose}>
-              Cancel
-            </Button>
-          </ModalFooter>
-        </Form>
+      <Modal 
+        open={isOpen} 
+        title={<div style={{textAlign: 'center'}}>Нова позиція</div>}
+        footer={[
+          <Button key="back">
+            Відмінити
+          </Button>,
+          <Button key="submit" type="primary">
+            Зберегти позицію
+          </Button>]}
+      >
+      <Form
+        name="marker"
+        layout="vertical">
+      <Form.Item
+      label="Ім'я позиції"
+      valuePropName="name"
+      rules={[{ required: true, message: 'Please input your username!' }]}>
+      <Input type='text' name='name' id='name' placeholder="Введіть ім'я позиції" />
+      </Form.Item>
+      <Form.Item label="Ворожа позиція" valuePropName="enemy">
+          <Switch />
+      </Form.Item>
+      <Form.Item
+      name="positionType"
+      label="Тип позиції"
+      valuePropName="positionType"
+      >
+      <Select
+      defaultValue="Піхота"
+      onChange={(value) => console.log(value) }
+      >
+        {Images.map((image, index) => (
+            <Select.Option key={index} value={index}>
+            <p>{image.name}</p>
+            <img src={image.image} className='image-position'/>
+            </Select.Option>
+        ))}
+      </Select>
+      </Form.Item>
+      <Form.Item  
+        name="positionCount"
+        label="Кількість"
+        valuePropName="positionCount">
+          <InputNumber />
+      </Form.Item>
+      <Form.Item  
+        name="description"
+        label="Додатковий опис"
+        valuePropName="description">
+          <TextArea autoSize={{minRows: 3, maxRows: 6}}  />
+      </Form.Item>
+      </Form>
       </Modal>
     </div>
   )
---
+++ src/images/index.jsx
@@ -0,0 +1,7 @@
+import allyArtillery from './ally-artillery.png'
+import allyInfantry from './ally-infantry.png'
+
+export default [
+    {image: allyArtillery, name: 'Артилерія'},
+    {image: allyInfantry, name: 'Піхота'}
+]
